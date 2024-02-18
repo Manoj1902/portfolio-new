dd4b8d0933b540b011b1ac5aed30bc9a2f2fb8d4
Manoj <Raj.manoj34@gmail.com>
2024-02-19 00:29:59 +0530

    second

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,6 +9,26 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 function Hero() {
   return (
     <section className={styles.container}>
+      <div className={styles.socialIcons}>
+        <a href="https://github.com/Manoj1902">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/manoj1902/">
+          <FaLinkedin />
+        </a>
+        <a href="https://www.instagram.com/lakra_manoj0/">
+          <FaInstagramSquare />
+        </a>
+        <a href="https://www.facebook.com/shmanoj1902/">
+          <FaFacebookSquare />
+        </a>
+        <a href="https://twitter.com/ManojK1902">
+          <FaSquareXTwitter />
+        </a>
+        <a href="https://api.whatsapp.com/send?phone=919818609640&text=Hi%2C%20I%20wanted%20to%20work%20with%20you">
+          <FaWhatsappSquare />
+        </a>
+      </div>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi</h1>
         <p className={styles.description}>I am Android Developer</p>
@@ -26,26 +46,7 @@ function Hero() {
         alt="profile-image"
         className={styles.heroImg}
       />
-      <div className={styles.socialIcons}>
-        <a href="">
-          <FaGithub />
-        </a>
-        <a href="">
-          <FaLinkedin />
-        </a>
-        <a href="">
-          <FaInstagramSquare />
-        </a>
-        <a href="">
-          <FaFacebookSquare />
-        </a>
-        <a href="">
-          <FaSquareXTwitter />
-        </a>
-        <a href="">
-          <FaWhatsappSquare />
-        </a>
-      </div>
+
       <div className={styles.topBlur}></div>
       <div className={styles.bottomBlur}></div>
     </section>
